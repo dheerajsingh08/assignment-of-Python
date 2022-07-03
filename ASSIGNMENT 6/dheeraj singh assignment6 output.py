c27580a1c6f3fb0ffccaf4c95c64a26515c6bf6d
dheerajsingh08 <104360266+dheerajsingh08@users.noreply.github.com>
2022-07-03 16:22:13 +0530

    Update dheeraj singh assignment6 output.py

--- ASSIGNMENT 6/dheeraj singh assignment6 output.py
+++ ASSIGNMENT 6/dheeraj singh assignment6 output.py
@@ -1,13 +1,12 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-==== RESTART: C:\Users\HP\Desktop\PYTHON ASSIGNMENTS\dheeraj assignment 6.py ===
+#QUESTION 1
 Enter a number: 7
 7  is not a perfect number
 
+#QUESTION 2
 Enter a String: madam
 madam is a palindrome
 
+#QUESTION 3
 Enter a number: 5
       1 
      1 1 
@@ -15,12 +14,15 @@ Enter a number: 5
    1 3 3 1 
   1 4 6 4 1 
 
+#QUESTION4
 Enter a String: the quick brown fox jumps over the lazy dog
 Yes 'the quick brown fox jumps over the lazy dog' is a panagram
 
+#QUESTION5
 Enter a hyphen separated sentence: red-brown-purple-yellow
 brown-purple-red-yellow
 
+#QUESTION6
 Student ID= 21102008
 Student ID= 21102008
 Student Name= Dheeraj Singh
@@ -28,6 +30,7 @@ Student ID= 21102008
 Student Name= Dheeraj Singh
 Student Class= 1st Year
 
+#QUESTION7
 True
 False
 True
@@ -37,6 +40,7 @@ Check whether the said classes are subclasses of the built-in object class or no
 True
 True
 
+#QUESTION8
 Enter number of elements : 6
 -4
 3
@@ -46,5 +50,6 @@ Enter number of elements : 6
 11
 -4 3 1
 None
+#QUESTION9
 Enter the sequence of parantheses : {}[]
 {}[] - is balanced
